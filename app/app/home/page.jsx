6ed4a9c7a48e8f612771cce8bb6835acd3a7383d
zhouxianping1 <390447296@qq.com>
2025-11-01 16:21:23 +0700

    Create page.jsx

---
+++ app/app/home/page.jsx
@@ -0,0 +1,9 @@
+// app/home/page.jsx
+export default function HomePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>ELT 真正的页面</h1>
+      <p>这里写你原来的内容，或者把 index.html 的内容搬过来。</p>
+    </main>
+  );
+}
